afe92738c318ba63801556cbacc337e8e20481ab
MultiFlex <wakanawardrobe@gmail.com>
2023-01-13 00:10:57 +0000

    complete test for capitalizing string

---
+++ capitalizeTest/capitalize.test.js
@@ -0,0 +1,13 @@
+const capitalize = require('./capitalize');
+
+test('capitalize first letter of string', () => {
+    expect(capitalize('john')).toBe('John');
+});
+
+test('capitalize first letter of string', () => {
+    expect(capitalize('ronald')).toBe('Ronald');
+});
+
+test('capitalize first letter of string', () => {
+    expect(capitalize('hello')).toBe('Hello');
+});
